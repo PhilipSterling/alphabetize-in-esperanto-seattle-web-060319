2193ebf19b138ad29c81203e96a2db629c5ea5d6
Philip Sterling <philipssterlings@gmail.com>
2019-06-07 15:38:40 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,10 @@
+require 'pry'
 def alphabetize(arr)
-  # code here
+  alpabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  alparray = alpabet.split(//)
+  i = 0
+  return arr.sort_by! do |x| 
+    end
 end
+alphabetize(["mi amas vin", "bonan matenon", "pacon", "ĉu vi parolas esperanton"])
+binding.pry
